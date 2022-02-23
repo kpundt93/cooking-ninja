bf94d3056640a8673366e272dd0278be34a81d13
Katie Pundt <kzpundt@gmail.com>
2022-02-22 20:39:10 -0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // page components
+import Navbar from'./components/Navbar'
 import Home from'./pages/home/Home'
 import Create from'./pages/create/Create'
 import Search from'./pages/search/Search'
@@ -11,20 +12,21 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/recipes/:id">
-            <Recipe />
-          </Route>
-        </Switch>
+        <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/recipes/:id">
+              <Recipe />
+            </Route>
+          </Switch>
       </BrowserRouter>
     </div>
   );
