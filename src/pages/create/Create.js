96f8f8d1f7f17179b91f8b1d8c3c30ecc7acb03a
Katie Pundt <kzpundt@gmail.com>
2022-02-23 16:28:44 -0800

    add ability to make POST requests

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { useFetch } from '../../hooks/useFetch'
+import { useHistory } from 'react-router-dom'
 // styles
 import './Create.css'
 
@@ -10,10 +12,13 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('');
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
+  const history = useHistory();
+
+  const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime, ingredients);
+    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes' });
   }
 
   const handleAdd = (e) => {
@@ -27,6 +32,13 @@ export default function Create() {
     ingredientInput.current.focus();
   }
 
+  // redirect user when we get data response
+  useEffect(() => {
+    if (data) {
+      history.push('/');
+    }
+  }, [data]);
+
   return (
     <div className="create">
       <h2 className="page-title">Add a New Recipe</h2>
@@ -37,11 +49,6 @@ export default function Create() {
         </label>
 
         <label>
-          <span>Recipe method:</span>
-          <textarea onChange={(e) => setMethod(e.target.value)} value={method} required />
-        </label>
-
-        <label>
           <span>Recipe ingredients:</span>
           <div className="ingredients">
             <input type="text" onChange={(e) => setNewIngredient(e.target.value)} value={newIngredient} ref={ingredientInput} />
@@ -49,7 +56,12 @@ export default function Create() {
           </div>
         </label>
         <p>Current ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)}</p> 
-
+        
+        <label>
+          <span>Recipe method:</span>
+          <textarea onChange={(e) => setMethod(e.target.value)} value={method} required />
+        </label>
+        
         <label>
           <span>Cooking time (minutes):</span>
           <input type="number" onChange={(e) => setCookingTime(e.target.value)} value={cookingTime} required />
