523bf4592c1d59be450299de6316060577f2d4bc
Katie Pundt <kzpundt@gmail.com>
2022-02-24 17:15:15 -0800

    create custom useTheme hook and context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
-import './Navbar.css';
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
+// components
 import SearchBar from './SearchBar'
+// styles
+import './Navbar.css';
 
 export default function Navbar() {
+  const { color } = useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
